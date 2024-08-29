15c8cdf53d8c546f3ccfc72a47927158b8b4898c
Renan <renan.henrique3439@gmail.com>
2024-08-29 11:59:31 -0300

    Add files via upload

---
+++ 03-aula03.py
@@ -0,0 +1,16 @@
+#Variáveis em Python
+#Não há necessidade de declará-las no início do programa
+#Basta criar no meio do algoritmo com a atribuição
+
+Nome = "Renan"
+print(Nome) #Imprime a string "Renan"
+print(type(Nome)) #Imprime o tipo da variável, String
+Idade = 16
+print(Idade) #Imprime o número 16
+print(type(Idade)) #Imprime o tipo da variável, que é int
+Altura = 1.74
+print(Altura) #Imprime o número 1.74
+print(type(Altura)) #Imprime o tipo da variável, que é Float
+teste = True
+print(teste) #Imprime o valor True
+print(type(teste)) #Imprime o tipo da variável, que é Bool
