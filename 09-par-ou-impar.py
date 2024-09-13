b052fbbfefc1893cf2127f6ceee2ffccece199c5
Renan <renan.henrique3439@gmail.com>
2024-09-13 10:28:08 -0300

    Update 09-par-ou-impar.py

--- 09-par-ou-impar.py
+++ 09-par-ou-impar.py
@@ -1,15 +1,6 @@
-print("Programa para Cálculo de IMC")
-peso = float(input("Digite o seu peso (em Kg): "))
-altura = float(input("Digite a sua altura (em metros): "))
-imc = peso / altura ** 2
+valor = int(input("Digite um valor: "))
 
-print(f"Seu IMC é {imc:.2f}.")
-print("Classificação do IMC: ")
-if imc < 18.5:
-    print("Abaixo do peso normal.")
-elif 18.5 <= imc < 25:
-    print("Peso normal.")
-elif 25 <= imc < 35:
-    print("Sobrepeso.")
+if valor % 2 == 0:
+    print("O número é par")
 else:
-    print("Obesidade")
+    print("O número é ímpar")
